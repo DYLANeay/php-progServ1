e383cca6e85280a6ae3774b67d0583a28e1cfc7b
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-04-02 09:50:36 +0200

    avancement dans le mini-projet

--- progserv1/mini-projet/functions.php
+++ progserv1/mini-projet/functions.php
@@ -1,25 +1,82 @@
 <?php
-function getPets()
-{
-    echo "Getting pets.<br>";
+
+$pets = [];
+
+
+// $pets = [
+//     "Caramel" => [
+//         "name" => "Caramel",
+//         "age" => 3,
+//     ],
+//     "Rex" => [
+//         "name" => "Rex",
+//         "age" => 8,
+//     ],
+//     'Tweety' => [
+//         'name' => 'Tweety',
+//         'age' => 1
+//     ],
+//     'Godzilla' => [
+//         'name' => 'Godzilla',
+//         'age' => 4
+//     ]
+// ];
+
+function getPets() {
+    global $pets;
+
+    return $pets;
 }
 
-function getPet($name)
-{
-    echo "Getting pet with name '$name'.<br>";
+function getPet($name) {
+
+    global $pets;
+
+    if (array_key_exists($name, $pets)) {
+        // ...si l'animal existe, on le retourne.
+        return $pets[$name];
+    } else {
+        // ...si l'animal n'existe pas, on retourne `false`.
+        return false;
+    }
 }
 
-function addPet($name, $age)
-{
-    echo "Adding $name, who is $age years old.<br>";
+function addPet($name, $age) {
+
+    global $pets;
+
+    $pets[$name] = [
+        'name' => $name,
+        'age' => $age
+    ];
+
+    return $pets[$name];
 }
 
-function updatePet($name, $age)
-{
-    echo "Updating pet with name '$name' to be $age years old.<br>";
+function updatePet($name, $age) {
+    global $pets;
+
+    if (array_key_exists($name, $pets)) {
+        // ...si l'animal existe, on le retourne.
+        $pet = $pets[$name];
+        $pet["age"] = $age;
+        $pets[$name] = $pet;
+        return $pet;
+    } else {
+        // ...si l'animal n'existe pas, on retourne `false`.
+        return false;
+    }
 }
 
-function removePet($name)
-{
-    echo "Removing pet with name '$name'.<br>";
+function removePet($name) {
+    global $pets;
+
+    if (array_key_exists($name, $pets)) {
+        // ...si l'animal existe, on le retourne.
+        unset($pets[$name]);
+        return true;
+    } else {
+        // ...si l'animal n'existe pas, on retourne `false`.
+        return false;
+    }
 }
--- progserv1/mini-projet/index.php
+++ progserv1/mini-projet/index.php
@@ -13,18 +13,36 @@ addPet("Tweety", 1);
 // Crée Godzilla, un lézard de 4 ans
 addPet("Godzilla", 4);
 
-// Récupère tous les animaux
-getPets();
-
 // Récupère l'animal nommé Rex
-getPet("Rex");
+$rex = getPet("Rex");
+
+// Affiche les propriétés de Rex
+print_r($rex);
+echo "<br>";
 
 // Met à jour l'âge de Rex à 9 ans
-updatePet("Rex", 9);
+$rex = updatePet("Rex", 9);
+
+// Affiche les propriétés de Rex
+print_r($rex);
+echo "<br>";
 
 // Supprime Tweety... :(
-removePet("Tweety");
+$success = removePet("Tweety");
 
+// Affiche si la suppression a réussi
+var_dump($success);
+echo "<br>";
+
+// On essaie de récupérer Tweety
+$tweety = getPet("Tweety");
+
+// Affiche si Tweety a été trouvé
+var_dump($tweety);
+echo "<br>";
+
+// Récupère tous les animaux
+$pets = getPets();
 ?>
 
 <!DOCTYPE html>
@@ -34,12 +52,32 @@ removePet("Tweety");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'accueil | Gestionnaire d'animaux de compagnie</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <h1>Page d'accueil du gestionnaire d'animaux de compagnie</h1>
     <p>Bienvenue sur la page d'accueil du gestionnaire d'animaux de compagnie !</p>
     <p>Utilise cette page pour visualiser et gérer tous les animaux de compagnie.</p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Âge</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($pets as $pet) { ?>
+                <tr>
+                    <td><?php echo $pet['name'] ?></td>
+                    <td><?= $pet['age'] ?> an(s)</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </body>
+
+
 
 </html>
