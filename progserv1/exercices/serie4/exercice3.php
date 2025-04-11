fbb44883efa5127ca762bb54e05d14a2002f2729
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-04-11 11:13:15 +0200

    ex4 done

--- progserv1/exercices/serie4/exercice3.php
+++ progserv1/exercices/serie4/exercice3.php
@@ -1,6 +1,9 @@
 <?php
-// Gère la soumission du formulaire sur une autre page
+
+
+// Gère la soumission du formulaire sur cette page
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
     $email = $_POST["email"];
@@ -16,40 +19,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulaire de contact</title>
 </head>
 
 <body>
+    <h1>Formulaire de contact</h1>
     <form action="exercice3.php" method="POST">
         <label for="firstName">Prénom :</label><br>
-        <input type="text" id="firstName" name="firstName">
+        <input type="text" id="firstName" name="firstName" value="<?php echo isset($firstName) ? $firstName : ''; ?>">
 
         <br>
 
         <label for="lastName">Nom :</label><br>
-        <input type="text" id="lastName" name="lastName">
+        <input type="text" id="lastName" name="lastName" value="<?php echo isset($lastName) ? $lastName : ''; ?>">
 
         <br>
 
         <label for="email">Adresse e-mail :</label><br>
-        <input type="email" id="email" name="email">
-
-        <br>
+        <input type="email" id="email" name="email" value="<?php echo isset($email) ? $email : ''; ?>">
 
         <br>
 
         <label for="message">Message :</label><br>
-        <textarea id="message" name="message"></textarea> <!-- attention ici à bien mettre l'élément textarea != attribut -->
+        <textarea id="message" name="message"><?php echo isset($message) ? $message : ''; ?></textarea>
 
         <br>
 
         <label for="consent">
-            <input type="checkbox" id="consent" name="consent">
+            <input type="checkbox" id="consent" name="consent" <?php echo isset($consent) && $consent ? 'checked' : ''; ?>>
             J'accepte le traitement de mes données personnelles
         </label>
 
@@ -57,8 +57,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <br>
 
         <button type="submit">Envoyer</button>
-        <button type="reset">Réinitialiser</button>
 
+        <!-- le reset ne fonctionne que si l'on a pas encore envoyé le formulaire !! sinon il revient à la valeur stockée -->
+        <button type="reset">Réinitialiser</button>
     </form>
 </body>
 
