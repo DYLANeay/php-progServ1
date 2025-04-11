19774f1ffeaf03856e8aeafcad09626f53124920
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-04-11 10:38:45 +0200

    exercice 1 done

---
+++ progserv1/exercices/serie4/exercice2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="contact.php" method="POST">
+        <label for="firstName">Prénom :</label><br>
+        <input type="text" id="firstName" name="firstName">
+
+        <br>
+
+        <label for="lastName">Nom :</label><br>
+        <input type="text" id="lastName" name="lastName">
+
+        <br>
+
+        <label for="email">Adresse e-mail :</label><br>
+        <input type="email" id="email" name="email">
+
+        <br>
+
+        <br>
+
+        <label for="message">Message :</label><br>
+        <textarea id="message" name="message"></textarea> <!-- attention ici à bien mettre l'élément textarea != attribut -->
+
+        <br>
+
+        <label for="consent">
+            <input type="checkbox" id="consent" name="consent">
+            J'accepte le traitement de mes données personnelles
+        </label>
+
+        <br>
+        <br>
+
+        <button type="submit">Envoyer</button>
+        <button type="reset">Réinitialiser</button>
+
+    </form>
+</body>
+
+</html>
