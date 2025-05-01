b602fb8c6e4752ca535a20048c0fe74e2e357a55
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-05-01 14:45:25 +0200

    mini projet done

--- progserv1/mini-projet/create.php
+++ progserv1/mini-projet/create.php
@@ -1,9 +1,8 @@
 <?php
+require 'functions.php';
+
 // Gère la soumission du formulaire
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "Le contenu de la variable `\$_POST` est : ";
-    var_dump($_POST);
-
     // Récupération des données du formulaire
     $name = $_POST["name"];
     $species = $_POST["species"];
@@ -52,6 +51,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($size) && (!is_numeric($size) || $size < 0)) {
         // On ajoute un message d'erreur au tableau
         array_push($errors, "La taille doit être un nombre entier positif.");
+    }
+
+    // Si le formulaire est valide, on ajoute l'animal
+    if (empty($errors)) {
+        // On ajoute l'animal à la base de données
+        $petId = addPet(
+            $name,
+            $species,
+            $nickname,
+            $sex,
+            $age,
+            $color,
+            $personalities,
+            $size,
+            $notes
+        );
+        // On redirige vers la page d'accueil avec tous les animaux
+        header("Location: view.php?id=" . $petId);
+        exit();
     }
 }
 ?>
--- progserv1/mini-projet/functions.php
+++ progserv1/mini-projet/functions.php
@@ -1,36 +1,105 @@
 <?php
 
-$pets = [];
+require "database.php";
 
 function getPets() {
-    global $pets;
+    // On utilise le mot-clé `global` pour accéder à la variable `$pdo`.
+    // Même si c'est une mauvaise pratique, c'est nécessaire ici.
+    global $pdo;
 
+    // On définit la requête SQL pour récupérer tous les animaux
+    $sql = "SELECT * FROM pets";
+
+    // On récupère tous les animaux
+    $pets = $pdo->query($sql);
+
+    // On transforme le résultat en tableau associatif
+    $pets = $pets->fetchAll();
+
+    // On transforme la chaîne `personalities` en tableau pour chaque animal
+    foreach ($pets as &$pet) { // On utilise `&` pour modifier l'élément dans le tableau `$pets`
+        if (!empty($pet['personalities'])) {
+            $pet['personalities'] = explode(',', $pet['personalities']);
+        }
+    }
+
+    // On retourne tous les animaux
     return $pets;
 }
 
-function getPet($name) {
+function getPet($id) {
+    // On utilise le mot-clé `global` pour accéder à la variable `$pdo`.
+    // Même si c'est une mauvaise pratique, c'est nécessaire ici.
+    global $pdo;
 
-    global $pets;
+    // On définit la requête SQL pour récupérer un animal
+    $sql = "SELECT * FROM pets WHERE id = '$id'";
 
-    if (array_key_exists($name, $pets)) {
-        // ...si l'animal existe, on le retourne.
-        return $pets[$name];
-    } else {
-        // ...si l'animal n'existe pas, on retourne `false`.
-        return false;
+    // On récupère l'animal spécifique
+    $pet = $pdo->query($sql);
+
+    // On transforme le résultat en tableau associatif
+    $pet = $pet->fetch();
+
+    // On transforme la chaîne `personalities` en tableau si elle existe
+    if ($pet && !empty($pet['personalities'])) {
+        $pet['personalities'] = explode(',', $pet['personalities']);
     }
+
+    // On retourne l'animal
+    return $pet;
 }
 
-function addPet($name, $age) {
 
-    global $pets;
+function addPet(
+    $name,
+    $species,
+    $nickname,
+    $sex,
+    $age,
+    $color,
+    $personalities,
+    $size,
+    $notes
+) {
+    // On utilise le mot-clé `global` pour accéder à la variable `$pdo`.
+    // Même si c'est une mauvaise pratique, c'est nécessaire ici.
+    global $pdo;
 
-    $pets[$name] = [
-        'name' => $name,
-        'age' => $age
-    ];
+    // On transforme le tableau `$personalities` en chaîne de caractères avec `implode`
+    $personalitiesAsString = implode(',', $personalities);
 
-    return $pets[$name];
+    // On définit la requête SQL pour ajouter un animal
+    $sql = "INSERT INTO pets (
+        name,
+        species,
+        nickname,
+        sex,
+        age,
+        color,
+        personalities,
+        size,
+        notes
+    ) VALUES (
+        '$name',
+        '$species',
+        '$nickname',
+        '$sex',
+        $age,
+        '$color',
+        '$personalitiesAsString',
+        $size,
+        '$notes'
+    )";
+
+    // On exécute la requête SQL pour ajouter un animal
+    $pdo->exec($sql);
+
+    // On récupère l'identifiant de l'animal ajouté
+    $petId = $pdo->lastInsertId();
+
+    // On retourne l'identifiant de l'animal ajouté.
+    return $petId;
 }
 
 function updatePet($name, $age) {
@@ -48,15 +117,15 @@ function updatePet($name, $age) {
     }
 }
 
-function removePet($name) {
-    global $pets;
 
-    if (array_key_exists($name, $pets)) {
-        // ...si l'animal existe, on le retourne.
-        unset($pets[$name]);
-        return true;
-    } else {
-        // ...si l'animal n'existe pas, on retourne `false`.
-        return false;
-    }
+function removePet($id) {
+    // On utilise le mot-clé `global` pour accéder à la variable `$pdo`.
+    // Même si c'est une mauvaise pratique, c'est nécessaire ici.
+    global $pdo;
+
+    // On définit la requête SQL pour supprimer un animal
+    $sql = "DELETE FROM pets WHERE id = '$id'";
+
+    // On exécute la requête SQL pour supprimer un animal
+    return $pdo->exec($sql);
 }
